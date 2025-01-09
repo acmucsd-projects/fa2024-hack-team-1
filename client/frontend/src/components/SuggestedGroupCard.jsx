f5ed66e26e702673d612f44a5556a3ebaf0c5d4c
Ethan Bautista <ethangb065@gmail.com>
2025-01-09 11:39:13 -0800

    Finished styling group cards

--- client/frontend/src/components/SuggestedGroupCard.jsx
+++ client/frontend/src/components/SuggestedGroupCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './SuggestedGroupCard.css';
-
+import { Box, Typography } from '@mui/material';
 function SuggestedGroupCard({
   name,
   thumbnailLink,
@@ -29,25 +28,52 @@ function SuggestedGroupCard({
   }
 
   return (
-    <div className="group-card">
-      <img
+    <Box sx={{
+        minHeight: '500px',          
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start', 
+        alignItems: 'flex-start',
+        '&:hover': {
+            filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 50%))",
+        },
+        willChange: 'filter',
+        transition: 'filter .1s ease-out',
+        width: 'fit-content',
+        border: ' 1px solid #003E33',
+        borderRadius: '15px',
+        backgroundColor: '#FFFFFF',
+        padding: '15px'
+
+    }}>
+        <img
         src={thumbnailLink}
         alt={`${name} Thumbnail`}
         className="group-card-thumbnail"
-      />
-      <h3 className="group-card-title">{name}</h3>
-      <p className="group-card-description">{description}</p>
-
-      <div className="group-card-info">
-        <p><strong>Location:</strong> {location}</p>
-        <p><strong>Budget:</strong> ${budget}</p>
-        <p><strong>People:</strong> {personCount}</p>
-        {/* Display the formatted start/end times if they exist */}
-        {formattedStart && formattedEnd && (
-          <p><strong>Dates:</strong> {formattedStart} – {formattedEnd}</p>
-        )}
-      </div>
-    </div>
+        style={{
+            width: '100%',
+            height: '150px',       
+            objectFit: 'cover',
+            borderRadius: '10px',
+        }}
+        />
+        <Typography variant="h3" display='block' sx={{ mt: '10px', }}>{name}</Typography>
+        <Box sx={{ width: '250px', height: '1px', bgcolor: '#003E33', mt: '15px', mb: '15px',}}></Box>
+        <Typography variant="p" display='block' sx={{ fontSize: '14px',}}>{description}</Typography>
+        <Box sx={{ width: '250px', height: '1px', bgcolor: '#003E33', mt: '15px', mb: '15px',}}></Box>
+        <Box sx={{
+            display: 'grid',
+            width: '100%',
+            gap: '5px'
+        }}>
+            <Typography variant="p" display='block' sx={{gridColumnStart: 1, fontSize: '15px' }}> <strong>{location}</strong></Typography>
+            <Typography variant="p" display='block' align='right' sx={{gridColumnStart: 2, fontSize: '15px' }}> ${budget} / Person</Typography>
+            <Typography variant="p" display='block' align='right' sx={{gridColumnStart: 2, gridRowStart: 2, fontSize: '15px' }}> <strong>People:</strong> {personCount}</Typography>
+        </Box>
+            {formattedStart && formattedEnd && (
+                <Typography variant="p" display='block' sx={{ margin: '10px auto', }}>{formattedStart} – {formattedEnd}</Typography>
+            )}
+    </Box>
   );
 }
 
