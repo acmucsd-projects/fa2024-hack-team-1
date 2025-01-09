fcfb89d6d05b5a9ccd0dba0150efbc03d353b82b
Ethan Bautista <ethangb065@gmail.com>
2025-01-09 12:01:29 -0800

    Slightly altered dividers and close button on RendevousDescBox

--- client/frontend/src/components/RendevousDescBox.jsx
+++ client/frontend/src/components/RendevousDescBox.jsx
@@ -79,7 +79,7 @@ function RendevousDescBox({ group, onClose }) {
               backgroundColor: "#F2F2F2",
             }}
           />
-          <Typography variant="body2" sx={{ lineHeight: "40px" }}>
+          <Typography variant="p" sx={{ lineHeight: "40px" }}>
             +10 others
           </Typography>
         </Box>
@@ -103,7 +103,7 @@ function RendevousDescBox({ group, onClose }) {
         </Button>
       </Box>
 
-      <Box sx={{ bgcolor: "#003033", width: "2px", height: "100%" }} />
+      <Box sx={{ bgcolor: "#003033", width: "1px", height: "100%" }} />
 
       <Box sx={{ width: "70%" }}>
         <Typography
@@ -115,7 +115,7 @@ function RendevousDescBox({ group, onClose }) {
         <Box
           sx={{
             width: "100%",
-            height: "2px",
+            height: "1px",
             backgroundColor: "#003033",
             mb: 3,
           }}
@@ -206,7 +206,7 @@ function RendevousDescBox({ group, onClose }) {
             mt: 3,
             backgroundColor: "#003E33",
             color: "#FFFFFF",
-            "&:hover": { backgroundColor: "#005F4F" },
+            "&:hover": { backgroundColor: "#005F4F", color: '#003E33'},
             width: "150px",
             height: "40px",
             alignSelf: "center",
